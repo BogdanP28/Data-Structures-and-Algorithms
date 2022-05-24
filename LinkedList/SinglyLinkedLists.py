b686ebe6aa74f1346d29e5cddbaf84de2fbd4850
Bogdan <bogdanp28@gmail.com>
2022-05-24 17:24:06 +0300

    Added additional functionalities: iterator, pop method

--- LinkedList/SinglyLinkedLists.py
+++ LinkedList/SinglyLinkedLists.py
@@ -16,11 +16,28 @@ class LinkedStack:
             """
             self._element = element
             self._next = next
+            
+    class LinkedListIterator:
+        def __init__(self, head):
+            self._current = head
+            
+        def __iter__(self):
+            return self
+        
+        def __next__(self):
+            if not self._current:
+                raise StopIteration
+            else:
+                item = self._current._element
+                self._current = self._current._next
+                return item
+            
     # ------------------------------- Stack methods -------------------------
     def __init__(self):
         """Create an empty stack"""
         self._head = None   # reference to the head node
         self._size = 0      # number of stack elements
+        self._current = self._head
         
     def __len__(self):
         """Return the number of elements in the stack"""
@@ -43,12 +60,38 @@ class LinkedStack:
             raise Exception('Stack is empty')
         return self._head._element
     
+    def pop(self):
+        """Remove and return the element from the top of the stack
+        Raise empty exception if the stack is empty
+        """
+        if self.is_empty():
+            raise Exception('Stack is empty')
+        answer = self._head._element
+        self._head = self._head._next        # Bypass the former top node
+        self._size -= 1
+        return answer
+    
+    def __iter__(self):
+        return self.LinkedListIterator(self._head) 
+    
+    def __str__(self):
+       
+       return '->'.join(str(x) for x in self)
+
+
+
+
+        
+    
 def main():
     lst = LinkedStack()
     for i in range(10):
         lst.push(i)
     print("Stack size: ", len(lst))
     print('Top', lst.top())
+    print('Pop', lst.pop())
+    print('Top', lst.top())
+    print(lst)
     
 if __name__ == '__main__':
     main()
